5a9e3ca766b0dd745d81498d7147db24b4fb8dc7
Suraj Verma <suraj.verma@gemsessence.com>
2018-06-22 18:51:38 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const GET_DOG = gql`
+  query {
+    dog(breed: "pavilion") {
+      id
+      breed
+      displayImage
+    }
+  }
+`
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    console.log(this.props);
+    return (
+      <Query query={GET_DOG} pollInterval={500}>
+     {({ loading, error, data, startPolling, stopPolling }) => {
+       if (loading) return <Text>Loading...</Text>;
+       if (error) return <Text>Error :(</Text>;
+
+       return (
+         <View>
+           <Image
+             source={{uri: data.dog.displayImage}}
+             style={{width: 100, height: 100}}
+           />
+           <Text>
+             {data.dog.breed}
+           </Text>
+         </View>
+       )
+     }}
+   </Query>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import App from './App';
+
+const client = new ApolloClient({
+  link: new HttpLink({uri: "https://nx9zvp49q7.lp.gql.zone/graphql"}),
+  cache: new InMemoryCache(),
+});
+
+const View = () => (
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent('Apollo_Client', () => View);
